bd31eb7a6d5d68a6004a5649f3491ae7ae950085
humberto raniolo <humberto.raniolo@gmail.com>
2020-11-25 23:39:46 -0400

    se añadio el parsing del json y guyardar en archivo un json

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import os
 import re
 import json
-from xml.dom.minidom import parse, parseString
+import xmltodict
 
 
 with open("SAPGW_Error_Log_20201123145005.xml","r",encoding="utf-8") as archivo:
     XmlString=archivo.read()
 XmlString= XmlString.replace("&","&amp;") 
 XmlString= XmlString.replace('<?xml version="1.0" encoding="utf-8"?>',"")
-Xml = parseString(XmlString)
-print(Xml)
+Xml = xmltodict.parse(XmlString)
+JsonString =str( Xml["SAPGW_ERROR_LOG"]["item"]["REQUEST_DATA"]["HTTP_BODY"])
+JsonString = JsonString.replace("&amp;","&")
+JsonSap = json.loads(JsonString)
+
+
+
+with open("JsonSap.json","w",encoding="utf-8") as file:
+			 json.dump(JsonSap,file,indent=4)
+
+print(JsonSap["jobReports"][2]["report"][0]["Type"])
