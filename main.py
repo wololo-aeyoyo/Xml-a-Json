bb1af453539f9c5d056ada61393d67acc6f94670
humberto raniolo <humberto.raniolo@gmail.com>
2020-11-25 23:50:14 -0400

    se comento el main.py y se añadio el archivo de requerimientos

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 import os
 import re
 import json
-import xmltodict
+import xmltodict 
 
-
+#se abre el archivo xml como un string
 with open("SAPGW_Error_Log_20201123145005.xml","r",encoding="utf-8") as archivo:
     XmlString=archivo.read()
+    
+#se elimina caracteres invalidos del string    
 XmlString= XmlString.replace("&","&amp;") 
 XmlString= XmlString.replace('<?xml version="1.0" encoding="utf-8"?>',"")
+
+#se convierte a objeto XML
 Xml = xmltodict.parse(XmlString)
-JsonString =str( Xml["SAPGW_ERROR_LOG"]["item"]["REQUEST_DATA"]["HTTP_BODY"])
+JsonString =str( Xml["SAPGW_ERROR_LOG"]["item"]["REQUEST_DATA"]["HTTP_BODY"]) #Se extra el Json del Xml
+
+#se deshace el cambio en del caracter invalido y se crea el objeto json
 JsonString = JsonString.replace("&amp;","&")
 JsonSap = json.loads(JsonString)
 
 
-
+#se guarda el json en disco 
 with open("JsonSap.json","w",encoding="utf-8") as file:
 			 json.dump(JsonSap,file,indent=4)
 
-print(JsonSap["jobReports"][2]["report"][0]["Type"])
+#print(JsonSap["jobReports"][2]["report"][0]["Type"])
+print("success!")
+input()
